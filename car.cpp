3eff4aaafca53475c16865dfbbc9927bcc71344c
michael0enkner <fmgalaxy.office@gmail.com>
2018-12-06 09:27:11 +0100

    First push

---
+++ car.cpp
@@ -0,0 +1,55 @@
+#include "car.h"
+
+struct Carimplementation
+{
+  enum CarType type;
+  enum Color color;
+  double fill_level;
+  double acceleration_rate;
+  int speed;
+};
+
+Car get_car(enum CarType cartype)
+{
+  return 0;
+}
+
+enum CarType get_type(Car car)
+{
+  return JEEP;
+}
+
+enum Color get_color(Car car)
+{
+  return BLACK;
+}
+
+void init()
+{
+  
+}
+
+double get_fill_level(Car car)
+{
+  return 0;
+}
+
+void accelerate(Car car)
+{
+
+}
+
+double get_acceleration_rate(Car car)
+{
+  return 0;
+}
+
+void set_acceleration_rate(Car car, double acceleration)
+{
+
+}
+
+int get_speed(Car car)
+{
+  return 0;
+}
--- car.h
+++ car.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------
-*				HTBLA-Leonding / Klasse: <your class>
+*				HTBLA-Leonding / Klasse: <2Dhif>
 * ---------------------------------------------------------
 * Exercise Number: 0
 * Title:			car.h
@@ -12,5 +12,27 @@
 */
 #ifndef ___CAR_H
 #define ___CAR_H
+
+typedef struct Carimplementation* Car;
+enum CarType {AIXAM,FIAT_MULTIPLA,JEEP};
+enum Color {RED,GREEN,BLUE,ORANGE,SILVER,BLACK};
+
+Car get_car(enum CarType cartype);
+
+enum CarType get_type(Car car);
+
+enum Color get_color(Car car);
+
+void init();
+
+double get_fill_level(Car car);
+
+double get_acceleration_rate(Car car);
+
+void set_acceleration_rate(Car car, double acceleration);
+
+void accelerate(Car car);
+
+int get_speed(Car car);
 
 #endif
